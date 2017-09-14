d72e104733a9c76e8205b010d522bab339e35d02
rllyy97 <rllyy97@gmail.com>
2017-09-14 16:21:17 -0500

    Added safeguard against trying to connect to stream with no internet (caused crash)

--- app/src/main/java/fm/kcou/kcou881fm/MainActivity.java
+++ app/src/main/java/fm/kcou/kcou881fm/MainActivity.java
@@ -1,36 +1,63 @@
 package fm.kcou.kcou881fm;
 // Author: Riley Evans, started September 13 2017
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url = "http://radio.kcou.fm:8180/stream"; // your URL here
+    String stream1 = "http://radio.kcou.fm:8180/stream"; // your URL here
+    String stream1Meta = "http://sc7.shoutcaststreaming.us:2199/rpc/c8180/streaminfo.get?x=1";
+    String stream2 = "";
+    String stream2Meta = "";
+
+
     MediaPlayer mediaPlayer = new MediaPlayer();
-    boolean playing = false;
+    int playing = 0; // 0=paused, 1=playing 2=connecting
+
+    public MainActivity() throws ParserConfigurationException, IOException, SAXException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
+        if(!isNetworkAvailable()){
+            findViewById(R.id.connectionWarning).setVisibility(View.VISIBLE);
+        }
     }
 
     public void playPauseStream(View v){
-        if(playing){
+        findViewById(R.id.connectionWarning).setVisibility(View.INVISIBLE);
+        if(playing==1){
             mediaPlayer.stop();
-            playing = false;
+            playing = 0;
             mediaPlayer.reset();
         }
-        else{
+        else if(isNetworkAvailable()){
             try {
-                mediaPlayer.setDataSource(url);
+                mediaPlayer.setDataSource(stream1);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -38,11 +65,40 @@ public class MainActivity extends AppCompatActivity {
             mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
                 public void onPrepared(MediaPlayer player){
                     player.start();
-                    playing = true;
+                    playing = 1;
+//                    getMetaData();
                 }
             });
+        }
+        else{
+            findViewById(R.id.connectionWarning).setVisibility(View.VISIBLE);
+            playing = 0;
         }
 
     }
 
+//    DocumentBuilderFactory dbf1 = DocumentBuilderFactory.newInstance();
+//    DocumentBuilder db1 = dbf1.newDocumentBuilder();
+//    Document metaDoc1 = db1.parse(new URL(stream1Meta).openStream());
+
+//    public void getMetaData(View v) throws ParserConfigurationException {
+//        NodeList trackList = metaDoc1.getElementsByTagName("track");
+//        Element artist = (Element) trackList.item(0);
+//        Element title = (Element) trackList.item(1);
+//        Element album = (Element) trackList.item(2);
+//
+//        TextView artistTextView = (TextView) findViewById(R.id.artistTextView);
+//        artistTextView.setText(artist.getTagName());
+//        TextView titleTextView = (TextView) findViewById(R.id.titleTextView);
+//        titleTextView.setText(title.getTagName());
+//        TextView albumTextView = (TextView) findViewById(R.id.albumTextView);
+//        albumTextView.setText(album.getTagName());
+//    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
