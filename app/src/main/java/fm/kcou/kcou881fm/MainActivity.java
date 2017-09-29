8e0bc2860fe51704650136ff5170d7fb5e4e8cdc
rllyy97 <rllyy97@gmail.com>
2017-09-29 12:41:03 -0500

    Successfully pulls JSON from url to device for track metadata

--- app/src/main/java/fm/kcou/kcou881fm/MainActivity.java
+++ app/src/main/java/fm/kcou/kcou881fm/MainActivity.java
@@ -1,51 +1,57 @@
 package fm.kcou.kcou881fm;
 // Author: Riley Evans, started September 13 2017
 import android.content.Context;
+
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     String stream1 = "http://radio.kcou.fm:8180/stream"; // your URL here
-    String stream1Meta = "http://sc7.shoutcaststreaming.us:2199/rpc/c8180/streaminfo.get?x=1.xml";
-    String stream2 = "";
-    String stream2Meta = "";
+    String stream1Meta = "http://sc7.shoutcaststreaming.us:2199/rpc/c8180/streaminfo.get"; ////////cut the string, take only what is after the ???
+//    String stream2 = "";
+//    String stream2Meta = "";
 
 
     MediaPlayer mediaPlayer = new MediaPlayer();
     int playing = 0; // 0=paused, 1=playing 2=connecting
-
-    public MainActivity() throws ParserConfigurationException, IOException, SAXException {
-    }
+//
+//    public MainActivity() throws ParserConfigurationException, IOException, SAXException {
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        if(!isNetworkAvailable()){
-            findViewById(R.id.connectionWarning).setVisibility(View.VISIBLE);
-        }
+//        if(!isNetworkAvailable()) {
+//            findViewById(R.id.connectionWarning).setVisibility(View.VISIBLE);
+//        }
+        MyAsyncTask getMeta = new MyAsyncTask();
+        getMeta.execute(stream1Meta);
+
     }
 
     public void playPauseStream(final View v){
@@ -55,55 +61,87 @@ public class MainActivity extends AppCompatActivity {
             playing = 0;
             mediaPlayer.reset();
             TextView playPauseButton = (TextView) findViewById(R.id.playButton);
-            playPauseButton.setText("Play Music");
+            playPauseButton.setText(getString(R.string.play));
         }
-        else if(isNetworkAvailable()){
+        else if(isNetworkAvailable()&&playing!=2){
+            playing = 2;
+            TextView playPauseButton = (TextView) findViewById(R.id.playButton);
+            playPauseButton.setText(getString(R.string.buffering));
             try {
                 mediaPlayer.setDataSource(stream1);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            TextView playPauseButton = (TextView) findViewById(R.id.playButton);
-            playPauseButton.setText("Buffering");
+
+
+
             mediaPlayer.prepareAsync();
             mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
                 public void onPrepared(MediaPlayer player){
                     player.start();
                     playing = 1;
                     TextView playPauseButton = (TextView) findViewById(R.id.playButton);
-                    playPauseButton.setText("Pause Music");
-//                    try {
-//                        getMetaData(v);
-//                    } catch (ParserConfigurationException e) {
-//                        e.printStackTrace();
-//                    }
+                    playPauseButton.setText(getString(R.string.pause));
+
                 }
             });
         }
-        else{
+        else if(!isNetworkAvailable()){
             findViewById(R.id.connectionWarning).setVisibility(View.VISIBLE);
             playing = 0;
         }
 
     }
 
-//    DocumentBuilderFactory dbf1 = DocumentBuilderFactory.newInstance();
-//    DocumentBuilder db1 = dbf1.newDocumentBuilder();
-//    Document metaDoc1 = db1.parse(new URL(stream1Meta).openStream()); // Causing crash on app open
-//
-//    public void getMetaData(View v) throws ParserConfigurationException {
-//        NodeList trackList = metaDoc1.getElementsByTagName("track");
-//        Element artist = (Element) trackList.item(0);
-//        Element title = (Element) trackList.item(1);
-//        Element album = (Element) trackList.item(2);
-//
-//        TextView artistTextView = (TextView) findViewById(R.id.artistTextView);
-//        artistTextView.setText(artist.getTagName());
-//        TextView titleTextView = (TextView) findViewById(R.id.titleTextView);
-//        titleTextView.setText(title.getTagName());
-//        TextView albumTextView = (TextView) findViewById(R.id.albumTextView);
-//        albumTextView.setText(album.getTagName());
-//    }
+    /////     Android Make API Call for json
+
+    private class MyAsyncTask extends AsyncTask<String, Void, Void> {
+
+        protected Void doInBackground(String... url) {
+            JSONObject json = null;
+            try {
+                json = readJsonFromUrl(url[0]);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                assert json != null;
+                System.out.println(json.get("id"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+            try {
+                InputStream is = new URL(url).openStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                String jsonText = readAll(rd);
+//                jsonText = jsonText.split("?")[1];
+                JSONObject json = new JSONObject(jsonText);
+                is.close();
+                return json;
+            } catch (IOException ex){
+                return null;
+            }
+        }
+
+        private String readAll(Reader rd) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while ((cp = rd.read()) != -1) {
+                sb.append((char) cp);
+            }
+            return sb.toString();
+        }
+
+    }
+
+
+
+    /////
 
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
